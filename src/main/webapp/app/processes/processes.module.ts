9ffaffb406b888ad0804356a5b710ca3a0e912c7
Yojhan <yojhanr95@gmail.com>
2019-10-24 14:28:27 -0500

    Crea plantilla de definicionees y actualiza clases de colores - Crea la plantilla de definiciones @templates/tpl-definiciones - Asigna estilos dinámicos de acuerdo al proceso seleccionado - Actualiza hoja de estilos global.scss para añadir clases de colores y fondos - Actualiza custom-variables scss con los colores definidos

--- src/main/webapp/app/processes/processes.module.ts
+++ src/main/webapp/app/processes/processes.module.ts
@@ -5,17 +5,21 @@ import { PROCESSES_ROUTE } from './processes.route';
 
 import { LukerSharedModule } from 'app/shared/shared.module';
 import { ProcessesComponent } from './processes.component';
+import { TplDefinicionesComponent } from './templates/tpl-definiciones/tpl-definiciones.component';
 import { VinculacionIntegracionComponent } from './vinculacion-integracion/vinculacion-integracion.component';
 import { VinculacionComponent } from './vinculacion-integracion/vinculacion/vinculacion.component';
 import { IntegracionComponent } from './vinculacion-integracion/integracion/integracion.component';
+import { DefVinculacionIntegracionComponent } from './vinculacion-integracion/definiciones/def-vinculacion-integracion.component';
 
 @NgModule({
     imports: [LukerSharedModule, RouterModule.forChild([PROCESSES_ROUTE])],
     declarations: [
         ProcessesComponent,
+        TplDefinicionesComponent,
         VinculacionIntegracionComponent,
         VinculacionComponent,
-        IntegracionComponent
+        IntegracionComponent,
+        DefVinculacionIntegracionComponent
     ]
 })
 export class LukerProcessesModule { }
