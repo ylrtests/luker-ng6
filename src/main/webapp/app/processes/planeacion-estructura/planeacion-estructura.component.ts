e1565d10e743b6b8b813762010f82c942689966e
Yojhan <yojhanr95@gmail.com>
2019-12-04 07:30:54 -0500

    Ajusta titulos de página en archivo processes.json y home.json

--- src/main/webapp/app/processes/planeacion-estructura/planeacion-estructura.component.ts
+++ src/main/webapp/app/processes/planeacion-estructura/planeacion-estructura.component.ts
@@ -38,6 +38,7 @@ export class PlaneacionEstructuraComponent implements OnInit {
           { title: 'Marcas', url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf' },
           { title: 'Marcas de canal', url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf' },
           { title: 'Aseo', url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf' },
+          { title: 'Comercial', url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf' },
           { title: 'CABAS', url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf' },
           { title: 'Logística', url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf' },
           { title: 'Talento & Gestión', url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf' }
